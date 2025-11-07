173a369e096550f6bca92f56fde120b137bfd23e
Kunal Madoliya <madoliyakunal2005@gmail.com>
2025-11-07 20:39:55 +0530

    learning

---
+++ intro.ts
@@ -0,0 +1,27 @@
+// var greetings : string = "kunal"
+
+// greetings = greetings.toUpperCase()
+// console.log(greetings)
+
+
+// let coin:string ;
+
+// function getCoin(){
+//     return "hello"
+// }
+
+// coin = getCoin()
+// console.log(coin);
+
+
+const heros = ["kunal" , "manvi"]
+// const heros = [1 , 2]
+
+
+heros.map((h):string =>{
+    return `hello ${h}`;
+    
+})
+
+export {}
+ 
